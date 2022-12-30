704f7be7fc40b2699ebbe73762b3bc579bc9687f
ZI-DESKTOP <zc3huang@uwaterloo.ca>
2022-12-30 01:27:57 -0500

    added ant generation

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const boardWidth = 400;
 const boardHeight = 400;
-const board_border = 'black';
-const board_background = "white";
+const boardBorder = "black";
+const boardBackground = "white";
+const antColor = "green";
+const antRadius = 1;
+const antNumber = 1;
 
 const board = document.getElementById("board");
 const boardContext = board.getContext("2d");
+let ants = [];
+
 resetCanvas();
 tick();
 
 function resetCanvas() {
     board.width = boardWidth;
     board.height = boardHeight;
-    boardContext.fillStyle = board_background;
-    boardContext.strokestyle = board_border;
+    boardContext.fillStyle = boardBackground;
+    boardContext.strokestyle = boardBorder;
     boardContext.fillRect(0, 0, board.width, board.height);
     boardContext.strokeRect(0, 0, board.width, board.height);
 }
 
-async function tick() {
-    setTimeout(function onTick() {
+class Ant {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        let dx = 0;
+        let dy = 0;
+    }
 
-        tick()
-    }, 100);
+    paint() {
+        boardContext.beginPath();
+        boardContext.ellipse(this.x, this.y, antRadius, antRadius, 0, 0, 360);
+        boardContext.strokestyle = antColor;
+        boardContext.stroke();
+    }
 }
 
 
+function generateAnts() {
+    for (let i = 0; i < antNumber; i++) {
+        ants.push(new Ant(Math.random() * boardWidth, Math.random() * boardHeight));
+    }
+
+    for (let ant of ants) {
+        ant.paint();
+    }
+}
+
+function tick() {
+    setTimeout(function onTick() {
+        generateAnts()
+        tick()
+    }, 100);
+}
